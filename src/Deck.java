3e8bb2e1c5d8ea2c42b32f08b4c6caa4f340a998
Conor James Giles <cgiles290@gmail.com>
2017-03-15 21:09:29 +0000

    Added Knuth Shuffle

--- src/Deck.java
+++ src/Deck.java
@@ -1,10 +1,15 @@
 import java.util.ArrayDeque;
 import java.util.List;
+import java.util.Random;
+
 public class Deck {
 	//Class for the storage of cards in a deck.
 	private ArrayDeque<Card> deck;
+	private Rand rand;
+	public static final Random gen = new Random();
 	Deck(){
 		deck = new ArrayDeque<Card>();
+		rand = new Rand();
 	}
 	public Card read(){
 		return deck.pop();
@@ -12,7 +17,14 @@ public class Deck {
 	public void replace(Card card){
 		deck.push(card);
 	}
-	public void shuffle(){
-		java.util.Collections.shuffle((List<?>) deck);
+	public void shuffle() {
+		Object[] array = deck.toArray();
+	    int n = array.length;
+	    while (n > 1) {
+	        int k = gen.nextInt(n--); //decrements after using the value
+	        Object temp = array[n];
+	        array[n] = array[k];
+	        array[k] = temp;
+	    }
 	}
 }
