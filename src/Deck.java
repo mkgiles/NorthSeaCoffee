6802799c21953958a46305bbbdd1c2fc7c4484bd
Conor James Giles <cgiles290@gmail.com>
2017-03-15 18:34:16 +0000

    Testing new card and deck methods

--- src/Deck.java
+++ src/Deck.java
@@ -1,8 +1,18 @@
 import java.util.ArrayDeque;
+import java.util.List;
 public class Deck {
 	//Class for the storage of cards in a deck.
-	ArrayDeque<Card> deck;
+	private ArrayDeque<Card> deck;
 	Deck(){
 		deck = new ArrayDeque<Card>();
+	}
+	public Card read(){
+		return deck.pop();
+	}
+	public void replace(Card card){
+		deck.push(card);
+	}
+	public void shuffle(){
+		java.util.Collections.shuffle((List<?>) deck);
 	}
 }
