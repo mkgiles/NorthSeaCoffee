10d9a866ea517d2a50503210f05324c0801151c8
Conor James Giles <cgiles290@gmail.com>
2017-03-23 22:18:36 +0000

    Shuffles are now random and safe

--- src/Deck.java
+++ src/Deck.java
@@ -7,13 +7,14 @@ public class Deck {
 	private static int[] seed;
 	//use ISAAC for random number generation.
 	public static void deckInit(){
-		rand = new Rand();
 		seed = new int[256];
 		for(int i=0;i<256;i++){
-			seed[i]=(int) (Math.random()*(0xFFFFFFFF)); //seed ISAAC with clock values.
+			seed[i]=(int) (Math.random()*(0xFFFFFF)); //seed ISAAC with clock values.
+			IO.putLine(""+seed[i]);
 		}
+		rand = new Rand(seed);
 		rand.Init(true);
-		rand.val();
+		rand.Isaac();
 	}
 	Deck(){
 		deck = new ArrayList<Card>();
@@ -31,10 +32,13 @@ public class Deck {
 	    int n = deck.size();
 	    while (n > 0) {
 	    	int r = Deck.rand.val();
-	        int k = Math.abs(r)%n--; //decrements after using the value
+	    	IO.putLine("Shuffle");
+	    	IO.putLine("" + r);
+	        int k = Math.abs(r%n--); //decrements after using the value
+	        IO.putLine(""+k);
 	        Card temp = deck.get(n);
+	        deck.add(deck.get(k));
 	        deck.remove(n);
-	        deck.add(n,deck.get(k));
 	        deck.remove(k);
 	        deck.add(k,temp);
 	    }
